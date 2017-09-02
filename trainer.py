f34ba16ecce192327ad222ec303c9b7ebef45b58
Your Name <you@example.com>
2017-09-02 18:26:24 +0800

    因為沒有 pre-trained weights 所以只好隨機初始化@@

--- trainer.py
+++ trainer.py
@@ -120,34 +120,34 @@ def custom_STFT_layer(x):
 stfted = Lambda(custom_STFT_layer, name='STFT')(input_layer)
 ## VGG19 Net:
 # Block 1
-x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(stfted)
-x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1', kernel_initializer='random_normal')(stfted)
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2', kernel_initializer='random_normal')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
 
 # Block 2
-x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1')(x)
-x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2')(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv1', kernel_initializer='random_normal')(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same', name='block2_conv2', kernel_initializer='random_normal')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
 
 # Block 3
-x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1')(x)
-x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
-x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
-x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv1', kernel_initializer='random_normal')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2', kernel_initializer='random_normal')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3', kernel_initializer='random_normal')(x)
+x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv4', kernel_initializer='random_normal')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
 
 # Block 4
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv2', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv3', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv4', kernel_initializer='random_normal')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
 
 # Block 5
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
-x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv1', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3', kernel_initializer='random_normal')(x)
+x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv4', kernel_initializer='random_normal')(x)
 x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
 model = Model(input_layer, x) ## model: wav -> features
