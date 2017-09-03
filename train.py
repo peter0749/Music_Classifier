d07a297ab0ac9a3cb784fa915076bf6cacf03e68
Your Name <you@example.com>
2017-09-04 01:16:47 +0800

    fix?

--- train.py
+++ train.py
@@ -40,10 +40,8 @@ parser.add_argument('--ar', type=int, default=11025, required=False,
                     help='Sample rate.')
 parser.add_argument('--lr', type=float, default=0.01, required=False,
                     help='Learning rate.')
-parser.add_argument('--offset_base', type=int, default=5, required=False,
-                    help='Time offset of base wav.')
-parser.add_argument('--offset_ref', type=int, default=5, required=False,
-                    help='Time offset of reference wav.')
+parser.add_argument('--batch_size', type=int, default=4, required=False,
+                    help='Batch size.')
 
 args = parser.parse_args()
 base_wav_path = args.base_wav_path
@@ -51,8 +49,7 @@ style_reference_wav_path = args.style_reference_wav_path
 iterations = args.iter
 rate = args.ar
 lr   = args.lr
-offset_base = args.offset_base
-offset_ref  = args.offset_ref
+batch_size = args.batch_size
 
 segment_n = 64
 FFT_n = 2048
@@ -60,13 +57,38 @@ FFT_t = FFT_n/8
 total_samp = segment_n*FFT_n
 img_nrows, img_ncols = 505, 768
 
-base_wav_data = preprocess_wav(base_wav_path, offset_base, total_samp, True)
-style_reference_wav_data = preprocess_wav(style_reference_wav_path, offset_ref, total_samp, True)
+def normalize_wav(wavData):
+    wavData = wavData.astype(np.float64) ## convert int32 -> float64
+    minAmp = wavData[np.argmin(np.abs(wavData))] ## find min amptitude
+    wavData[:] -= minAmp ## shift wave to center (a.k.a. 0)
+    maxAmp = max(1e-7, np.max(np.abs(wavData))) ## find max amptitude
+    wavData[:] /= maxAmp ## normalize to (-1, 1)
+    return wavData
 
-data = np.concatenate((base_wav_data, style_reference_wav_data), axis=0)
-label= np.zeros((2, 2), dtype=np.bool)
-label[0,0] = True
-label[1,1] = True
+def generator():
+    base_rate, base_wav_data = scipy.io.wavfile.read(base_wav_path)
+    ref_rate , ref_wav_data  = scipy.io.wavfile.read(style_reference_wav_path)
+    assert base_rate==ref_rate and base_rate==rate
+    if base_wav_data.ndim>1:
+        base_wav_data = base_wav_data[...,0]
+    if ref_wav_data.ndim>1:
+        ref_wav_data = ref_wav_data[...,0]
+    base_wav_data = normalize_wav(base_wav_data)
+    ref_wav_data  = normalize_wav(ref_wav_data)
+    base_wav_data = base_wav_data.astype(np.float32)
+    ref_wav_data  = ref_wav_data.astype(np.float32)
+    data_shape = (batch_size, total_samp, 1)
+    label_shape= (batch_size, 2)
+    fetch_size = batch_size*total_samp
+    y_base = np.zeros(label_shape, dtype=np.bool)
+    y_ref  = np.zeros(label_shape, dtype=np.bool)
+    y_base[:,0] = 1
+    y_ref[:,1]  = 1
+    while True:
+        for i in xrange(0, len(base_wav_data)-fetch_size):
+            yield np.reshape(base_wav_data[i:i+fetch_size], data_shape), y_base
+        for i in xrange(0, len(ref_wav_data)-fetch_size):
+            yield np.reshape(ref_wav_data[i:i+fetch_size], data_shape), y_ref
 
 model = vgg19_sound.vgg19(input_tensor = None,
                           segment_n = segment_n,
@@ -78,8 +100,7 @@ model = vgg19_sound.vgg19(input_tensor = None,
 model.summary()
 optimizer = RMSprop(lr=lr)
 model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-model.fit(data, label, batch_size=2, shuffle=False, epochs=iterations)
-
+model.fit_generator(generator(), steps_per_epoch=1, epochs=iterations)
 to_save = vgg19_sound.vgg19(input_tensor = None,
                           segment_n = segment_n,
                           FFT_n = FFT_n,
